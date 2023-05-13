7b29bb99614a6a74be5ef301d8c3ee06ace47980
Ching Yaw Hao <ching.yh@mydidian.com>
2023-05-13 22:08:27 +0800

    feat(help): added light cone to commands

--- src/client/constants/pompom.ts
+++ src/client/constants/pompom.ts
@@ -18,4 +18,15 @@ export default (prefix:string):Help[] => [{
     `Tips:\n` +
     `This command has shortform \`${prefix}c\` for easier access`
   ]
+}, {
+  commands: ['light', 'lightcone'],
+  category: 'HONKAI STAR RAIL',
+  description: 'Show you detail about a light cone',
+  examples: ['lightcone <lightcone name>', 'lightcone <path name>'],
+  mentionOnly: false,
+  notes: [
+    `If you don't put any query bot will show you menu to browse our light cone gallery`,
+    `Tips:\n` +
+    `This command has shortform \`${prefix}l\` for easier access`
+  ]
 }]
